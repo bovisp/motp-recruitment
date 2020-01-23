03e97d16b1366563ed12679e2e6356436c2878ac
Bovis <Paul.Bovis2@ec.gc.ca>
2020-01-23 12:23:44 -0700

    Finish adding right hand side tab of tephi image map interaction

--- resources/js/components/imagemap/ImageMapTephi.vue
+++ resources/js/components/imagemap/ImageMapTephi.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="row mb-5" style="height: 450px;">
+  <div class="row mb-5" style="max-height: 450px;">
     <div class="col-md-5">
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 643 433">
         <image width="643" height="433" xlink:href="https://res.cloudinary.com/tcddmedia/image/upload/v1579799943/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/12z_sounding_locations_awuolm.jpg"></image>
         <a 
-          @click.prevent="showMetar(station)"
+          @click.prevent="showTabs(station)"
           v-for="station in stations"
           :key="station.code"
           xlink:href="#"
@@ -24,59 +24,105 @@
       </svg>
     </div>
 
-    <div class="col-md-7" style="height: 100%; overflow: hidden; overflow-y:scroll;">
-      <p>
-        <small v-html="metar.toUpperCase()" class="font-weight-bold"></small>
-      </p>
+    <div class="col-md-7">
+      <div v-if="Object.keys(activeStation).length">
+        <ul id="Case2E1Tab4" class="nav nav-tabs">
+          <li class="nav-item">
+            <a id="tephiTab-tab" class="nav-link active" href="#tephiTab" data-toggle="tab">Tephigram</a>
+          </li>
+          <li class="nav-item">
+            <a id="hodoTab-tab" class="nav-link" href="#hodoTab" data-toggle="tab">Hodograph</a>
+          </li>
+        </ul>
+
+        <!-- Tab panes for case1Tab4 -->
+        <div 
+          class="tab-content mb-4 rounded"
+          style="border-left: 1px solid #dee2e6; border-right: 1px solid #dee2e6; border-bottom: 1px solid #dee2e6; margin-top: -10px;"
+        >
+          <div id="tephiTab" class="tab-pane active">
+            <img :src="activeStation.images.tephi" class="w-75 pt-4 px-2 pb-2">
+          </div>
+          
+          <div id="hodoTab" class="tab-pane">
+            <img :src="activeStation.images.hodo" class="w-75 pt-4 px-2 pb-2">
+          </div>
+        </div>
+      </div>
     </div>
   </div> 
 </template>
 
 <script>
-import metarData from '../../data/metars-one' 
+import { find } from 'lodash-es'
 
 export default {
   data () {
     return {
-      metars: [],
-      metar: '',
-      stations: [
-        { x: 145, y: 70, name: 'Sept-Iles', code: 'cyzv', images: {
-          tephi: '',
-          hodo: ''
-        } },
-        { x: 342, y: 165, name: 'Stephenville', code: 'cyjt', images: {
-          tephi: '',
-          hodo: ''
-        } },
-        { x: 501, y: 205, name: 'St. John\'s', code: 'cyyt', images: {
-          tephi: '',
-          hodo: ''
-        } },
-        { x: 71, y: 196, name: 'Caribou', code: 'kpqi', images: {
-          tephi: '',
-          hodo: ''
-        } },
-        { x: 103, y: 247, name: 'Gagetown', code: 'cycx', images: {
-          tephi: '',
-          hodo: ''
-        } },
-        { x: 103, y: 331, name: 'Yarmouth', code: 'cyqi', images: {
-          tephi: '',
-          hodo: ''
-        } }
-      ]
+      activeStation: {},
+      stations: [{ 
+        x: 145,
+        y: 70,
+        name: 'Sept-Iles',
+        code: 'cyzv',
+        images: {
+          tephi: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013434/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYZV_SEPT_ILES_f6gtqq.png',
+          hodo: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013434/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYZV_SEPT_ILES_HODO_qzkwd5.png'  
+        }
+      },
+      { 
+        x: 342,
+        y: 165,
+        name: 'Stephenville',
+        code: 'cyjt',
+        images: {
+          tephi: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYJT_STEPHENVILLE_z9tltf.png',
+          hodo: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYJT_STEPHENVILLE_HODO_dnzjsa.png'
+        } 
+      },{ 
+        x: 501,
+        y: 205,
+        name: 'St. John\'s',
+        code: 'cyyt', 
+        images: {
+          tephi: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013434/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYYT_ST_JOHN_S_WEST_clbc4p.png',
+          hodo: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYYT_ST_JOHN_S_WEST_HODO_xtn24n.png'
+        } 
+      },{ 
+        x: 71,
+        y: 196,
+        name: 'Caribou',
+        code: 'kpqi',
+        images: {
+          tephi: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013434/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/KCAR_CARIBOU_fvda3u.png',
+          hodo: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013435/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/KCAR_CARIBOU_HODO_xds5er.png'
+        } 
+      },{ 
+        x: 103,
+        y: 247,
+        name: 'Gagetown',
+        code: 'cycx',
+        images: {
+          tephi: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYCX_GAGETOWN_HODO_uajkae.png',
+          hodo: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYCX_GAGETOWN_HODO_uajkae.png  '
+        } 
+      },{ 
+        x: 103,
+        y: 331,
+        name: 'Yarmouth',
+        code: 'cyqi',
+        images: {
+          tephi: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYQI_YARMOUTH_ouk3tb.png',
+          hodo: 'https://res.cloudinary.com/tcddmedia/image/upload/v1576013433/moip_direct_entry_assessment/case%202/Exercise%201/Soundings%20-%20Actual/CYQI_YARMOUTH_HODO_blmmfq.png'
+        } 
+      }]
     }
   },
 
   methods: {
-    showMetar (station) {
-      this.metar = this.metars[station.code]
+    showTabs (station) {
+      this.activeStation = find(this.stations, ['code', station.code])
     }
-  },
-
-  mounted () {
-    this.metars = metarData
   }
 }
 </script>
