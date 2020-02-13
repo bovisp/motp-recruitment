9a6b726fef9d293b7f3602be7b01c3e2e2ecce68
Paul Bovis <paul.bovis@canada.ca>
2020-02-13 12:48:00 -0700

    Correct fog climatology images in case 1

--- resources/views/cases/case-one.blade.php
+++ resources/views/cases/case-one.blade.php
@@ -170,12 +170,12 @@
     >
       <div id="case1Tab4YTSFog" class="tab-pane active">
         <p class="pt-3 px-3">{{ __('case1.tab.4.caption.1') }}</p>
-        <img class="w-75" src="https://res.cloudinary.com/tcddmedia/image/upload/v1581103193/moip_direct_entry_assessment/case%201/YTS-FOG-CLIMATOLOGY_jdvff6.png" alt="" />
+        <img class="w-75" src="https://res.cloudinary.com/tcddmedia/image/upload/v1581623120/moip_direct_entry_assessment/case%201/YTS-FOG-CLIMATOLOGY_qbghg8.png" alt="" />
       </div>
       
       <div id="case1Tab4YYUFog" class="tab-pane">
         <p class="pt-3 px-3">{{ __('case1.tab.4.caption.2') }}</p>
-        <img class="w-75" src="https://res.cloudinary.com/tcddmedia/image/upload/v1581448910/moip_direct_entry_assessment/case%201/YYU-FOG-CLIMATOLOGY_czizr8.png" alt="" />
+        <img class="w-75" src="https://res.cloudinary.com/tcddmedia/image/upload/v1581623120/moip_direct_entry_assessment/case%201/YYU-FOG-CLIMATOLOGY_k39dfm.png" alt="" />
       </div>
     </div>
 
