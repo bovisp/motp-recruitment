f110603cb30879a70d09018ed607e379e6fcb1b0
Paul Bovis <paul.bovis@canada.ca>
2020-03-12 16:11:10 -0600

    Corrected table values in answers show route

--- resources/views/answers/partials/c2e2table.blade.php
+++ resources/views/answers/partials/c2e2table.blade.php
@@ -23,11 +23,11 @@
     <tr>
       <th scope="row">{{ __('components.qpf-table.precipitation-amount') }}</th>
       <td>{{ $answers->napat15 }}</td>
-      <td>{{ $answers->napat15 }}</td>
-      <td>{{ $answers->napat15 }}</td>
-      <td>{{ $answers->napat15 }}</td>
-      <td>{{ $answers->napat15 }}</td>
-      <td>{{ $answers->napat15 }}</td>
+      <td>{{ $answers->napat18 }}</td>
+      <td>{{ $answers->napat21 }}</td>
+      <td>{{ $answers->napat00 }}</td>
+      <td>{{ $answers->napat03 }}</td>
+      <td>{{ $answers->napat06 }}</td>
     </tr>
   </tbody>
 </table>
@@ -48,20 +48,20 @@
     <tr>
       <th scope="row">{{ __('components.qpf-table.precipitation-type') }}</th>
       <td>{{ $answers->dfptt15 }}</td>
-      <td>{{ $answers->dfptt15 }}</td>
-      <td>{{ $answers->dfptt15 }}</td>
-      <td>{{ $answers->dfptt15 }}</td>
-      <td>{{ $answers->dfptt15 }}</td>
-      <td>{{ $answers->dfptt15 }}</td>
+      <td>{{ $answers->dfptt18 }}</td>
+      <td>{{ $answers->dfptt21 }}</td>
+      <td>{{ $answers->dfptt00 }}</td>
+      <td>{{ $answers->dfptt03 }}</td>
+      <td>{{ $answers->dfptt06 }}</td>
     </tr>
     <tr>
       <th scope="row">{{ __('components.qpf-table.precipitation-amount') }}</th>
       <td>{{ $answers->dfpat15 }}</td>
-      <td>{{ $answers->dfpat15 }}</td>
-      <td>{{ $answers->dfpat15 }}</td>
-      <td>{{ $answers->dfpat15 }}</td>
-      <td>{{ $answers->dfpat15 }}</td>
-      <td>{{ $answers->dfpat15 }}</td>
+      <td>{{ $answers->dfpat18 }}</td>
+      <td>{{ $answers->dfpat21 }}</td>
+      <td>{{ $answers->dfpat00 }}</td>
+      <td>{{ $answers->dfpat03 }}</td>
+      <td>{{ $answers->dfpat06 }}</td>
     </tr>
   </tbody>
 </table>
